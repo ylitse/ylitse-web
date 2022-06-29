45ad80e928b5f6bb3993265f66544c3a85a508fd
Antti Niinikoski <antti.niinikoski@robu.fi>
2022-06-29 12:47:56 +0300

    Move e2e folder under cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
