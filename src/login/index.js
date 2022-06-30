08b25755f054539f30a1959e719f536a0cce0b67
Antti Niinikoski <antti.niinikoski@robu.fi>
2022-06-30 16:29:17 +0300

    Add static login page

---
+++ src/login/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+(function (window, document) {
+    var form = document.forms.namedItem('login');
+
+    form.addEventListener('submit', function(event) {
+        fetch('/api/weblogin', {
+            method: 'POST',
+            body: new FormData(form),
+            credentials: 'include'
+        }).then(function(response) {
+            if (response.ok) {
+                return response.json();
+            }
+            var loginError = document.getElementById('login-error');
+            loginError.style.display = "block";
+            form.reset();
+            form.elements[0].focus();
+            throw new TypeError('Couldn\'t log in.');
+        }).then(function(data) {
+            window.location.href = '/';
+        }).catch(function(error) {
+            console.log(error.message);
+        });
+
+        event.preventDefault();
+    }, false);
+}(window, document));
