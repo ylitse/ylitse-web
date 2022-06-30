1f1d7011c1b56589bd5734b896cf69cc9f74d1e1
Helvi <helvi.lainekallio@robu.fi>
2022-06-30 13:20:12 +0300

    rebase changes

---
+++ src/features/Navigation/Navigation.styles.tsx
@@ -0,0 +1,87 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const StyledLogoText = styled.div`
+  font-family: 'Baloo 2', cursive;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+	color: white;
+  flex: 1;
+
+	max-width: fit-content;
+`;
+
+export const StyledLogo = styled.img`
+  height: 40px;
+	margin-left: 12%;
+	margin-right: 1rem;
+`;
+
+export const StyledNavBarItem = styled.div`
+  flex: 1;
+  color: white;
+	max-width: fit-content;
+	height: 60px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	
+`;
+
+export const StyledNavLink = styled(NavLink)`
+	color: white;
+	font-family: 'Baloo 2', cursive;
+  font-style: normal;
+  font-weight: 400;
+	text-decoration: none;
+	height: 60px;
+	line-height: 60px;
+	div {
+		margin-left:	.5rem;
+		margin-right: .5rem;
+		padding-left: .5rem;
+		padding-right: .5rem;
+	}
+	&:active {
+		background-color: #43BFFF;
+		color: #1C325D;
+		div {
+			height: 30px;
+			border-bottom: 1px solid #1C325D;
+		}
+	}
+	&.active {
+    background-color: #43BFFF;
+		color: #1C325D;
+		div {
+			height: 40px;
+			border-bottom: 1px solid #1C325D;
+		}
+  }
+`;
+
+export const StyledNavBar = styled.div`
+  width: 100%;
+  height: 60px;
+  z-index: 10;
+  background-color: #4a2acb;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+	position: relative;
+`;
+
+export const StyledNavBarItems = styled.div`
+	flex: 1;
+	margin-right: 12%;
+	display: flex;
+	justify-content: flex-end;
+`;
--- src/features/Navigation/Navigation.tsx
+++ src/features/Navigation/Navigation.tsx
@@ -1,9 +1,22 @@
-function NavigationBar() {
-	return (
-		<>
-			<h1>Rekisteröidy</h1>
-		</>
-	);
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { StyledNavBar, StyledLogo, StyledNavLink, StyledLogoText, StyledNavBarItems } from './Navigation.styles';
+import Logo from './logo.svg';
+
+const NavigationBar = () => {
+  return (
+    <StyledNavBar>
+			<StyledLogo src={Logo} />
+      <StyledLogoText>YLITSE</StyledLogoText>
+      <StyledNavBarItems>
+			<StyledNavLink to='/' className="navbar-link"><div>Koti</div></StyledNavLink>
+			<StyledNavLink to='/' className="navbar-link"><div>Mentorit</div></StyledNavLink>
+			<StyledNavLink to='/' className="navbar-link"><div>Chat</div></StyledNavLink>
+			<StyledNavLink to='/' className="navbar-link"><div>Info</div></StyledNavLink>
+			<StyledNavLink to='/' className="navbar-link"><div>Kirjaudu ulos</div></StyledNavLink>
+      </StyledNavBarItems>
+    </StyledNavBar>
+  );
 };
 
 export default NavigationBar;
