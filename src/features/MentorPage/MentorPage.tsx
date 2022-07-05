5533ae536024cfd50d57db56b5ef86f750d064ca
Helvi <helvi.lainekallio@robu.fi>
2022-07-05 10:35:48 +0300

    Fixed static file serving. Layout finished.

--- src/features/MentorPage/MentorPage.tsx
+++ src/features/MentorPage/MentorPage.tsx
@@ -4,6 +4,7 @@ import {
   StyledMentorHeader,
   StyledMentorInfoText,
   StyledMentorSearchDiv,
+  StyledMentorCardContainer,
 } from './MentorPage.styles';
 import MentorSkills from '../MentorSkills';
 import MentorSearch from '../MentorSearch';
@@ -15,13 +16,15 @@ const MentorPage = () => {
         <StyledMentorHeader>Mentorit</StyledMentorHeader>
         <StyledMentorSearchDiv>
           <StyledMentorInfoText>
-            Tervetuloa selaamaan mentoreiden profiileja! Löydät uusimmat mentorit listassa ensimmäisenä. Hakutoimintoa käyttämällä
-            voit rajata mentoreita ongelmasi perusteella
+            Tervetuloa selaamaan mentoreiden profiileja! Löydät uusimmat
+            mentorit listassa ensimmäisenä. Hakutoimintoa käyttämällä voit
+            rajata mentoreita ongelmasi perusteella
           </StyledMentorInfoText>
-          <MentorSearch/>
-          <MentorSkills/>
+          <MentorSearch />
+          <MentorSkills />
         </StyledMentorSearchDiv>
       </StyledMentorSearchElement>
+      <StyledMentorCardContainer></StyledMentorCardContainer>
     </StyledMentorPage>
   );
 };
