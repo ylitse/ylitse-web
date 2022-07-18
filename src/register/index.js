43867aa2ed681fb2b721d9692836f17cc13213f9
Markus Kuosmanen <markus.kuosmanen@robu.fi>
2022-07-18 19:09:26 +0300

    implement registration page design and functionality

--- src/register/index.js
+++ src/register/index.js
@@ -28,3 +28,35 @@
     false,
   );
 })(window, document);
+
+function togglePassword() {
+  var passwordInput = document.getElementById('password');
+  var passwordHint = document.getElementById('retrieve-password');
+  if (passwordInput.getAttribute('type') === 'password') {
+    passwordInput.type = 'text';
+    passwordHint.innerHTML = 'Piilota salasana';
+  } else {
+    passwordInput.type = 'password';
+    passwordHint.innerHTML = 'Näytä salasana';
+  }
+}
+
+function checkForm() {
+  let requiredFieldsFilled = true;
+  document
+    .getElementById('register-form')
+    .querySelectorAll('[required]')
+    .forEach(function (field) {
+      if (!requiredFieldsFilled) return;
+      if (!field.checkValidity()) {
+        console.log(field);
+        requiredFieldsFilled = false;
+        return;
+      }
+    });
+  if (requiredFieldsFilled) {
+    document.getElementById('submit-button').disabled = false;
+  } else {
+    document.getElementById('submit-button').disabled = true;
+  }
+}
