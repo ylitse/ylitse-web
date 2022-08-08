765d1f151355826ea140f075cbd192e5a74bcb2e
Markus Kuosmanen <markus.kuosmanen@robu.fi>
2022-08-08 19:55:42 +0300

    add requirements-based e2e tests for password confirmation field

--- cypress/tests/register.cy.ts
+++ cypress/tests/register.cy.ts
@@ -3,6 +3,8 @@ describe('register', () => {
     cy.get('input[name="username"]').type('exampleUsername');
   const addPassword = () =>
     cy.get('input[name="password"]').type('examplePassword');
+  const addPasswordConfirmation = () =>
+    cy.get('input[name="password-confirmation"]').type('examplePassword');
   const addScreenName = () =>
     cy.get('input[name="screen-name"]').type('exampleScreenName');
   const checkRequiredAge = () =>
@@ -13,6 +15,7 @@ describe('register', () => {
   const fillOutForm = () => {
     addUsername();
     addPassword();
+    addPasswordConfirmation();
     addScreenName();
     checkRequiredAge();
     checkPrivacyConsent();
@@ -43,6 +46,35 @@ describe('register', () => {
     cy.get('input[name="password"]').should('have.value', 'examplePassword');
   });
 
+  it('hides password confirmation as default', () => {
+    addPasswordConfirmation();
+    cy.get('input[name="password-confirmation"]').should(
+      'not.contain',
+      'examplePassword',
+    );
+  });
+
+  it('shows password confirmation after toggle click', () => {
+    addPasswordConfirmation();
+    cy.get('button[id="password-confirmation-toggle"]').click();
+    cy.get('input[name="password-confirmation"]').should(
+      'have.value',
+      'examplePassword',
+    );
+  });
+
+  it('shows error message if passwords are different', () => {
+    addPassword();
+    cy.get('input[name="password-confirmation"]').type('wrongPassword').blur();
+    cy.contains('Salasanat eivät täsmää').should('be.visible');
+  });
+
+  it('shows no error message if passwords are the same', () => {
+    addPassword();
+    addPasswordConfirmation().blur();
+    cy.contains('Sähköpostiosoite on virheellinen').should('not.be.visible');
+  });
+
   it('shows error message if invalid email', () => {
     cy.get('input[name="email"]').type('email').blur();
     cy.contains('Sähköpostiosoite on virheellinen').should('be.visible');
@@ -55,6 +87,7 @@ describe('register', () => {
 
   it('prevents registration if username field is empty', () => {
     addPassword();
+    addPasswordConfirmation();
     addScreenName();
     checkRequiredAge();
     checkPrivacyConsent();
@@ -63,6 +96,16 @@ describe('register', () => {
 
   it('prevents registration if password field is empty', () => {
     addUsername();
+    addPasswordConfirmation();
+    addScreenName();
+    checkRequiredAge();
+    checkPrivacyConsent();
+    submitShouldBeDisabled();
+  });
+
+  it('prevents registration if password confirmation field is empty', () => {
+    addUsername();
+    addPassword();
     addScreenName();
     checkRequiredAge();
     checkPrivacyConsent();
@@ -72,6 +115,7 @@ describe('register', () => {
   it('prevents registration if screen name field is empty', () => {
     addUsername();
     addPassword();
+    addPasswordConfirmation();
     checkRequiredAge();
     checkPrivacyConsent();
     submitShouldBeDisabled();
@@ -80,6 +124,7 @@ describe('register', () => {
   it('prevents registration if required age toggle is off', () => {
     addUsername();
     addPassword();
+    addPasswordConfirmation();
     addScreenName();
     checkPrivacyConsent();
     submitShouldBeDisabled();
@@ -88,6 +133,7 @@ describe('register', () => {
   it('prevents registration if privacy consent toggle is off', () => {
     addUsername();
     addPassword();
+    addPasswordConfirmation();
     addScreenName();
     checkRequiredAge();
     submitShouldBeDisabled();
