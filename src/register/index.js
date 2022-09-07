c74d3f14f8b45e63425fe3dd90a99a074af9e730
Markus Kuosmanen <markus.kuosmanen@robu.fi>
2022-09-07 18:30:28 +0300

    Reorganize registration form validation to improve understandability
    Refactor form validation to use newer JavaScript (async/await) to fix flickering submit button
    
    Update registration form code to use newer EcmaScript features for consistency
    
    Remove unnecessary default value
    
    Correct wrong password length for adding input checkmark
    
    Add missing error removal function
    
    Simplify password visibility toggle
    
    Simplify handling empty input fields
    
    Add comments to structure registration form code
    
    Fix asynchronicity bug
    
    Shorten variable name

--- src/register/index.js
+++ src/register/index.js
@@ -1,21 +1,14 @@
 /* eslint-disable */
-function handleErrors(response) {
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
-  return response;
-}
+((window, document) => {
+  const form = document.forms.namedItem('register');
 
-(function (window, document) {
-  var form = document.forms.namedItem('register');
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
+    const formData = new FormData(form);
 
-  form.addEventListener(
-    'submit',
-    function (event) {
-      var formData = new FormData(form);
-      var createdUser;
-
-      fetch('/api/accounts', {
+    try {
+      // Create a new account
+      const createAccountResponse = await fetch('/api/accounts', {
         method: 'POST',
         body: JSON.stringify({
           password: formData.get('password'),
@@ -29,167 +22,150 @@ function handleErrors(response) {
         headers: {
           'Content-Type': 'application/json',
         },
-      })
-        .then(handleErrors)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          createdUser = data.user;
+      });
+      const accountData = await createAccountResponse.json();
+      const createdUser = accountData.user;
 
-          return fetch('/api/login', {
-            method: 'POST',
-            body: JSON.stringify({
-              login_name: formData.get('username'),
-              password: formData.get('password'),
-            }),
-            credentials: 'include',
-            headers: { 'Content-Type': 'application/json' },
-          });
-        })
-        .then(handleErrors)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          return fetch('/api/users/' + createdUser.id, {
-            method: 'PUT',
-            body: JSON.stringify({
-              display_name: formData.get('display-name'),
-              role: createdUser.role,
-              account_id: createdUser.account_id,
-              id: createdUser.id,
-            }),
-            credentials: 'include',
-            headers: {
-              Authorization: 'Bearer ' + data.tokens.access_token,
-              'Content-Type': 'application/json',
-            },
-          });
-        })
-        .then(handleErrors)
-        .then(function () {
-          window.location.replace('/login');
-        })
-        .catch(function (error) {
-          console.error(error.message);
-        });
-      event.preventDefault();
-    },
-    false,
-  );
+      // Log in using the created account
+      const loginResponse = await fetch('/api/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          login_name: formData.get('username'),
+          password: formData.get('password'),
+        }),
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const loginData = await loginResponse.json();
+
+      // Update the user by adding the display name
+      const updateUserResponse = await fetch(`/api/users/${createdUser.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          display_name: formData.get('display-name'),
+          role: createdUser.role,
+          account_id: createdUser.account_id,
+          id: createdUser.id,
+        }),
+        credentials: 'include',
+        headers: {
+          Authorization: `Bearer ${loginData.tokens.access_token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      // Redirect to login page
+      if (updateUserResponse.ok) {
+        window.location.replace('/login');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
 })(window, document);
 
-function isUsernameFree(username) {
-  return fetch('/api/search?login_name=' + username, { method: 'HEAD' }).then(
-    function (response) {
-      return response.status === 204;
-    },
-  );
-}
+const isUsernameFree = async username => {
+  const response = await fetch('/api/search?login_name=' + username, {
+    method: 'HEAD',
+  });
+  return response.status === 204;
+};
 
-function togglePasswordInput(inputId, toggleId) {
-  var passwordInput = document.getElementById(inputId);
-  var passwordToggle = document.getElementById(toggleId);
-  if (passwordInput.getAttribute('type') === 'password') {
-    passwordInput.type = 'text';
-    passwordToggle.innerHTML = 'Piilota salasana';
+const toggleInput = id => {
+  const input = document.getElementById(id);
+  const toggle = document.getElementById(`${id}-toggle`);
+  if (input.getAttribute('type') === 'password') {
+    input.type = 'text';
+    toggle.innerHTML = 'Piilota salasana';
   } else {
-    passwordInput.type = 'password';
-    passwordToggle.innerHTML = 'Näytä salasana';
+    input.type = 'password';
+    toggle.innerHTML = 'Näytä salasana';
   }
-}
+};
 
-function togglePassword() {
-  togglePasswordInput('password', 'password-toggle');
-}
+const getInputError = id =>
+  document.getElementById(id).parentElement.querySelector('.input-error');
 
-function togglePasswordConfirmation() {
-  togglePasswordInput('password-confirmation', 'password-confirmation-toggle');
-}
+const displayError = input => {
+  input.classList.add('error-border');
+  document.querySelector(`label[for=${input.id}]`).classList.add('error-color');
+  getInputError(input.id).style.display = 'flex';
+};
 
-function getInputError(inputId) {
-  return document
-    .getElementById(inputId)
-    .parentElement.querySelector('.input-error');
-}
-
-function clearError(field) {
-  if (field.type !== 'checkbox') {
-    field.classList.remove('error-border');
-    document
-      .querySelector(`label[for=${field.id}]`)
-      .classList.remove('error-color');
-    getInputError(field.id).style.display = 'none';
-  }
-}
-
-function displayError(field) {
-  if (field.value.length > 0) {
-    field.classList.add('error-border');
-    document
-      .querySelector(`label[for=${field.id}]`)
-      .classList.add('error-color');
-    getInputError(field.id).style.display = 'flex';
-  }
-  field.classList.remove('input-checkmark');
-}
-
-function checkForm() {
-  var formError = false;
+const removeError = input => {
+  input.classList.remove('error-border');
   document
+    .querySelector(`label[for=${input.id}]`)
+    .classList.remove('error-color');
+  getInputError(input.id).style.display = 'none';
+};
+
+const checkForm = async () => {
+  let formError = false;
+  const inputs = document
     .getElementById('register-form')
-    .querySelectorAll('input')
-    .forEach(function (field) {
-      if (field.id === 'username') {
-        // Username must be at least 2 characters long
-        if (field.value.length >= 2) {
-          var username = document.getElementById(field.id).value;
-          isUsernameFree(username).then(function (isUsernameFree) {
-            if (isUsernameFree) {
-              clearError(field);
-              field.classList.add('input-checkmark');
-            } else {
-              formError = true;
-              getInputError('username').innerHTML =
-                'Käyttäjätunnus on jo käytössä';
-              displayError(field);
-              document.getElementById('submit').disabled = formError;
-            }
-          });
-        } else if (field.value.length === 1) {
-          formError = true;
-          getInputError('username').innerHTML = 'Käyttäjätunnus on liian lyhyt';
-          displayError(field);
-          document.getElementById('submit').disabled = formError;
+    .querySelectorAll('input');
+
+  // Validate all inputs
+  for (const input of inputs) {
+    if (!input.value && input.type !== 'checkbox') {
+      // Text input is empty
+      if (input.id !== 'email') {
+        // A missing email is not an error
+        formError = true;
+      }
+      removeError(input);
+      input.classList.remove('input-checkmark');
+    } else if (input.id === 'username') {
+      if (input.checkValidity()) {
+        // Username is long enough
+        const username = document.getElementById(input.id).value;
+        const usernameIsFree = await isUsernameFree(username);
+        if (usernameIsFree) {
+          removeError(input);
+          input.classList.add('input-checkmark');
         } else {
-          clearError(field);
-          field.classList.remove('input-checkmark');
-        }
-      } else if (field.id === 'password-confirmation') {
-        var passwordsMatch =
-          field.value === document.getElementById('password').value;
-        if (passwordsMatch) {
-          clearError(field);
-          if (field.value.length >= 5) {
-            field.classList.add('input-checkmark');
-          }
-        } else {
+          // Username is taken
           formError = true;
-          displayError(field);
-        }
-      } else if (field.checkValidity()) {
-        clearError(field);
-        if (field.value.length > 0) {
-          field.classList.add('input-checkmark');
+          input.classList.remove('input-checkmark');
+          getInputError('username').innerHTML = 'Käyttäjätunnus on jo käytössä';
+          displayError(input);
         }
       } else {
+        // Username is too short
         formError = true;
-        if (field.type !== 'checkbox') {
-          displayError(field);
-        }
+        input.classList.remove('input-checkmark');
+        getInputError('username').innerHTML = 'Käyttäjätunnus on liian lyhyt';
+        displayError(input);
       }
-    });
-  // Submit button is disabled if there is a form error
+    } else if (input.id === 'password-confirmation') {
+      if (input.value === document.getElementById('password').value) {
+        // Passwords match
+        removeError(input);
+        if (input.value.length >= 8) {
+          input.classList.add('input-checkmark');
+        }
+      } else {
+        // Passwords don't match
+        formError = true;
+        displayError(input);
+        input.classList.remove('input-checkmark');
+      }
+    } else if (input.checkValidity()) {
+      // Input value is valid
+      if (input.type !== 'checkbox') {
+        removeError(input);
+        input.classList.add('input-checkmark');
+      }
+    } else {
+      // Input value is invalid
+      formError = true;
+      if (input.type !== 'checkbox') {
+        displayError(input);
+        input.classList.remove('input-checkmark');
+      }
+    }
+  }
+  // Disable submit button if any input is invalid
   document.getElementById('submit').disabled = formError;
-}
+};
