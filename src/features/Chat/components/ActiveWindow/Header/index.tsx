0469b89c831ebd6e5b2107b3aab9bce7c61a6e04
Markus Kuosmanen <markus.kuosmanen@robu.fi>
2023-11-21 00:02:34 +0200

    Make Chat window header responsive

--- src/features/Chat/components/ActiveWindow/Header/index.tsx
+++ src/features/Chat/components/ActiveWindow/Header/index.tsx
@@ -13,9 +13,14 @@ import ArchivedIcon from '@/static/icons/archived-chats.svg';
 import Buttons from './Buttons';
 import BlockedIcon from '@/static/icons/blocked-chats.svg';
 import { IconButton } from '@/components/Buttons';
-import { palette } from '@/components/variables';
+import { CONTENT_WIDTH, palette } from '@/components/variables';
 import { Profile as ProfileIcon } from '@/components/Icons/Profile';
-import { LARGE_ROW_HEIGHT, ROW_HEIGHT } from '@/features/Chat/constants';
+import {
+  CHAT_GAP_WIDTH,
+  CHAT_MENU_WIDTH,
+  LARGE_ROW_HEIGHT,
+  ROW_HEIGHT,
+} from '@/features/Chat/constants';
 import Text from '@/components/Text';
 
 type Props = {
@@ -41,75 +46,70 @@ const Header = ({ chat }: Props) => {
   };
 
   return (
-    <Container isTablet={isTablet}>
+    <Container mobile={isMobile} tablet={isTablet}>
       {isTablet && (
         <IconButton variant="back" sizeInPx={40} onClick={returnToTabletMenu} />
       )}
-      {mentor && (
-        <ProfileInfo>
-          {icons[chat.status]}
-          {isMobile ? (
-            <Texts>
-              <MentorName variant="h2">{chat.displayName}</MentorName>
-              <MentorBio isMobile variant="p">
-                {'Jutellaanko? <3'}
-                {/* {mentor.statusMessage} */}
-              </MentorBio>
-            </Texts>
-          ) : (
-            <>
-              <MentorName variant="h2">{chat.displayName}</MentorName>
-              <MentorBio variant="p">
-                {'Jutellaanko? <3'}
-                {/* {mentor.statusMessage} */}
-              </MentorBio>
-            </>
-          )}
-        </ProfileInfo>
-      )}
-      <Buttons chat={chat} tabletMode={isTablet} />
+      <IconContainer>{icons[chat.status]}</IconContainer>
+      <MentorName variant="h2">{mentor?.name}</MentorName>
+      <MentorBio isMobile={isMobile} isTablet={isTablet} variant="p">
+        {mentor?.statusMessage}
+      </MentorBio>
+      <ButtonsWrapper>
+        <Buttons chat={chat} tabletMode={isTablet} />
+      </ButtonsWrapper>
     </Container>
   );
 };
 
-// Siirrä haku Buttons-komponenttiin
-
-const Container = styled.div<{ isTablet: boolean }>`
+const Container = styled.div<{ mobile: boolean; tablet: boolean }>`
+  align-items: center;
   border-bottom: 1px solid ${palette.greyLight};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.03);
   box-sizing: border-box;
   display: flex;
   gap: 30px;
-  height: ${({ isTablet }) => (isTablet ? LARGE_ROW_HEIGHT : ROW_HEIGHT)};
-  justify-content: space-between;
+  height: ${({ tablet }) => (tablet ? LARGE_ROW_HEIGHT : ROW_HEIGHT)};
+  justify-content: flex-start;
   padding: 14px 40px;
+  width: ${({ mobile, tablet }) =>
+    mobile
+      ? '100vw'
+      : tablet
+      ? '100vw'
+      : `calc(${CONTENT_WIDTH}-${CHAT_MENU_WIDTH}-${CHAT_GAP_WIDTH}})`};
 `;
 
-const ProfileInfo = styled.div`
-  align-items: center;
-  display: flex;
-`;
-
-const Texts = styled.div`
-  align-items: flex-start;
-  display: flex;
-  flex-direction: column;
+const IconContainer = styled.div`
+  flex-shrink: 0;
 `;
 
 const MentorName = styled(Text)`
-  display: block;
   margin: 0;
-  padding-left: 20px;
+  max-width: calc(
+    (
+      ${CONTENT_WIDTH}-${CHAT_MENU_WIDTH}-${CHAT_GAP_WIDTH}-
+        (2 * 40px + 49px + 20px + 2 * 30px + 500px + 30px)
+    )
+  );
+  overflow: hidden;
+  text-overflow: ellipsis;
   white-space: nowrap;
 `;
 
-const MentorBio = styled(Text)<{ isMobile?: boolean }>`
-  flex-shrink: 1;
+const MentorBio = styled(Text)<{ isMobile?: boolean; isTablet?: boolean }>`
   margin: 0;
+  max-width: ${({ isMobile, isTablet }) =>
+    isMobile || isTablet
+      ? 'calc(100vw - 2*40px - 49px - 3*40px - 200px - 4*30px - 20px - 100px)'
+      : `calc((${CONTENT_WIDTH}-${CHAT_MENU_WIDTH}-${CHAT_GAP_WIDTH}-(2 * 40px + 49px + 20px + 2*30px + 500px))/2)`};
   overflow: hidden;
-  padding-left: ${isMobile => (isMobile ? '20px' : '30px')};
   text-overflow: ellipsis;
   white-space: nowrap;
+`;
+
+const ButtonsWrapper = styled.div`
+  margin-left: auto;
 `;
 
 export default Header;
