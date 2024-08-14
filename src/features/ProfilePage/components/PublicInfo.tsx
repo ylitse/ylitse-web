ad02219ca1a6f49a7079b9c29d09b2ddac339049
Markus Kuosmanen <markus.kuosmanen@robu.fi>
2024-08-14 11:18:39 +0300

    Add public column elements

--- src/features/ProfilePage/components/PublicInfo.tsx
+++ src/features/ProfilePage/components/PublicInfo.tsx
@@ -10,18 +10,37 @@ const PublicInfo = () => {
   return (
     <Container>
       <Text variant="h2">{t('public.title')}</Text>
+      <SaveNotice>{t('public.mentor.saveNotice')}</SaveNotice>
+      <Form>
+        <Text>{t('public.mentor.mandatoryNotice')}</Text>
+      </Form>
     </Container>
   );
 };
 
 const Container = styled.div`
+  align-items: center;
   background-color: ${palette.white};
   border-radius: 10px;
   box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.2);
   box-sizing: border-box;
   display: flex;
   flex: 2;
-  padding: 3rem;
+  flex-direction: column;
+  padding-top: 3rem;
+`;
+
+const SaveNotice = styled(Text)`
+  align-items: center;
+  background-color: ${palette.blue3};
+  display: flex;
+  height: 2.5rem;
+  justify-content: center;
+  width 100%;
+`;
+
+const Form = styled.div`
+  padding: 0 3rem;
 `;
 
 export default PublicInfo;
