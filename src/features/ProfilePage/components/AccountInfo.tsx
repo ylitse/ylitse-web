8b863993d5e2191c6fcc81e7b46ac358bfb7539a
Markus Kuosmanen <markus.kuosmanen@robu.fi>
2024-08-23 17:47:16 +0300

    Finish up Profile page layout and styling

--- src/features/ProfilePage/components/AccountInfo.tsx
+++ src/features/ProfilePage/components/AccountInfo.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 
-import { TextButton } from '@/components/Buttons';
+import { IconButton, TextButton } from '@/components/Buttons';
 import { palette } from '@/components/variables';
+import { Profile as ProfileIcon } from '@/components/Icons/Profile';
 import Text from '@/components/Text';
 
 import type { UserRole } from '@/features/Authentication/userSlice';
@@ -14,27 +15,48 @@ type Props = {
 const AccountInfo = ({ role }: Props) => {
   const { t } = useTranslation('profile');
 
+  const editPassword = () => {
+    console.log('Edit password');
+  };
+
+  const editEmail = () => {
+    console.log('Edit email');
+  };
+
   return (
     <Container>
       <Text variant="h2">{t('account.title')}</Text>
       <Section>
         <Text variant="label">{t('account.roles.title')}</Text>
-        <Text>{t(`account.roles.${role}`)}</Text>
+        <RoleRow>
+          <ProfileIcon color="purpleDark" />
+          <Text>{t(`account.roles.${role}`)}</Text>
+        </RoleRow>
       </Section>
       <Section>
         <Text variant="label">{t('account.username')}</Text>
         <Text>Maija19283192</Text>
       </Section>
       <Section>
-        <Text variant="label">{t('account.password')}</Text>
-        <Text>************</Text>
+        <Row>
+          <Column>
+            <Text variant="label">{t('account.password')}</Text>
+            <Text>************</Text>
+          </Column>
+          <IconButton variant="edit" sizeInPx={48} onClick={editPassword} />
+        </Row>
       </Section>
       <Section>
-        <Text variant="label">{t('account.email')}</Text>
-        <Text>maija1923983@gmail.com</Text>
-        <Text>{t('account.emailInfo')}</Text>
+        <Row>
+          <Column>
+            <Text variant="label">{t('account.email')}</Text>
+            <Text>maija1923983@gmail.com</Text>
+          </Column>
+          <IconButton variant="edit" sizeInPx={48} onClick={editEmail} />
+        </Row>
+        <Text variant="blueBox">{t('account.emailInfo')}</Text>
       </Section>
-      <TextButton variant="danger">{t('account.delete')}</TextButton>
+      <DeleteButton variant="danger">{t('account.delete')}</DeleteButton>
     </Container>
   );
 };
@@ -53,8 +75,30 @@ const Container = styled.div`
 const Section = styled.div`
   border-bottom: 1px solid ${palette.blueDark};
   display: flex;
+  flex-direction: column;
+  padding-bottom: 1rem;
+  padding-top: 2rem;
+`;
+
+const RoleRow = styled.div`
+  align-items: center;
+  display: flex;
   gap: 1rem;
-  padding: 2rem 2rem 2rem 0;
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const DeleteButton = styled(TextButton)`
+  align-self: center;
+  margin-top: 2rem;
 `;
 
 export default AccountInfo;
--- src/features/ProfilePage/components/PublicInfo.tsx
+++ src/features/ProfilePage/components/PublicInfo.tsx
@@ -16,6 +16,7 @@ const PublicInfo = () => {
   const [birthYear, setBirthYear] = useState('');
   const [area, setArea] = useState('');
   const [status, setStatus] = useState('');
+  const [isAbsent, setIsAbsent] = useState<boolean>(true);
   const [story, setStory] = useState('');
   const [topicSearchValue, setTopicSearchValue] = useState('');
 
@@ -24,6 +25,7 @@ const PublicInfo = () => {
   const updateBirthYear = (birthYear: string) => setBirthYear(birthYear);
   const updateArea = (area: string) => setArea(area);
   const updateStatus = (status: string) => setStatus(status);
+  const toggleIsAbsent = () => setIsAbsent(isAbsent => !isAbsent);
   const updateStory = (story: string) => setStory(story);
 
   return (
@@ -32,42 +34,66 @@ const PublicInfo = () => {
       <SaveNotice>{t('public.mentor.saveNotice')}</SaveNotice>
       <Form>
         <Text>{t('public.mentor.mandatoryNotice')}</Text>
-        <LabeledInput
-          label={t('public.displayName')}
-          onChange={updateDisplayName}
-          value={displayName}
-        />
-        <LabeledInput
-          label={t('public.mentor.birthYear')}
-          onChange={updateBirthYear}
-          value={birthYear}
-        />
-        <LabeledInput
-          label={t('public.mentor.area')}
-          onChange={updateArea}
-          value={area}
-        />
-        <LabeledInput
-          label={t('public.mentor.status')}
-          onChange={updateStatus}
-          value={status}
-        />
-        <Text variant="label">{t('public.mentor.absence.title')}</Text>
-        <Text>{t('public.mentor.absence.info')}</Text>
+        <DoubleColumn>
+          <Column>
+            <LabeledInput
+              label={t('public.displayName')}
+              onChange={updateDisplayName}
+              value={displayName}
+            />
+            <LabeledInput
+              label={t('public.mentor.birthYear')}
+              onChange={updateBirthYear}
+              value={birthYear}
+            />
+            <LabeledInput
+              label={t('public.mentor.area')}
+              onChange={updateArea}
+              value={area}
+            />
+          </Column>
+          <Column>
+            <LabeledInput
+              label={t('public.mentor.status')}
+              onChange={updateStatus}
+              value={status}
+            />
+            <Text variant="label">{t('public.mentor.absence.title')}</Text>
+            <Row>
+              <AbsenceSwitch
+                id="isAbsent"
+                type="checkbox"
+                checked={isAbsent}
+                onChange={toggleIsAbsent}
+              />
+              <Text inputId="isAbsent" variant="label">
+                {t(`public.mentor.absence.switch.${isAbsent ? 'on' : 'off'}`)}
+              </Text>
+            </Row>
+            <Text variant="blueBox">{t('public.mentor.absence.info')}</Text>
+          </Column>
+        </DoubleColumn>
         <Text variant="label">{t('public.mentor.story')}</Text>
-        <TextInput variant="textarea" onChange={updateStory} value={story} />
-        <Text variant="label">{t('public.mentor.topics')}</Text>
-        <TopicSearch
-          variant="iconInput"
-          color={topicSearchValue ? 'blueDark' : 'greyFaded'}
-          leftIcon={{
-            sizeInPx: searchInputIconSize,
-            variant: 'search',
-          }}
-          onChange={setTopicSearchValue}
-          placeholder={t('public.mentor.addTopic')}
-          value={topicSearchValue}
+        <StoryInput
+          variant="textarea"
+          onChange={updateStory}
+          rows={4}
+          value={story}
         />
+        <Label variant="label">{t('public.mentor.topics')}</Label>
+        <SearchBar>
+          <TopicSearch
+            variant="iconInput"
+            color={topicSearchValue ? 'blueDark' : 'greyFaded'}
+            leftIcon={{
+              sizeInPx: searchInputIconSize,
+              variant: 'search',
+            }}
+            onChange={setTopicSearchValue}
+            placeholder={t('public.mentor.addTopic')}
+            value={topicSearchValue}
+          />
+        </SearchBar>
       </Form>
     </Container>
   );
@@ -100,9 +126,103 @@ const Form = styled.div`
   padding: 0 3rem;
 `;
 
-const TopicSearch = styled(TextInput)`
+const DoubleColumn = styled.div`
+  display: flex;
+  gap: 3rem;
+`;
+
+const Column = styled.div`
+  display: flex;
   flex: 1;
-  max-width: 400px;
+  flex-direction: column;
+`;
+
+const Row = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
+`;
+
+const AbsenceSwitch = styled.input``;
+
+// const Slider = styled.span``;
+
+// /* The switch - the box around the slider */
+// .switch {
+//   position: relative;
+//   display: flex;
+//   width: 60px;
+//   height: 30px;
+// }
+
+// /* Hide default HTML checkbox */
+// .switch input {
+//   opacity: 0;
+//   width: 0;
+//   height: 0;
+// }
+
+// /* The slider */
+// .slider {
+//   border: 2px solid;
+//   border-color: #616161;
+//   border-radius: 20px;
+//   position: absolute;
+//   cursor: pointer;
+//   top: 0;
+//   left: 0;
+//   right: 0;
+//   bottom: 0;
+//   background-color: white;
+//   -webkit-transition: 0.4s;
+//   transition: 0.4s;
+// }
+
+// .slider:before {
+//   background-color: #616161;
+//   border-radius: 50%;
+//   position: absolute;
+//   content: '';
+//   height: 22px;
+//   width: 22px;
+//   left: 4px;
+//   bottom: 2px;
+//   -webkit-transition: 0.2s;
+//   transition: 0.2s;
+// }
+
+// input:checked + .slider {
+//   border-color: #4a2acb;
+// }
+
+// input:checked + .slider:before {
+//   background-color: #4a2acb;
+//   -webkit-transform: translateX(26px);
+//   -ms-transform: translateX(26px);
+//   transform: translateX(26px);
+// }
+
+const StoryInput = styled(TextInput)`
+  margin-top: 0.5rem;
+`;
+
+const Label = styled(Text)`
+  margin-top: 1rem;
+`;
+
+const SearchBar = styled.div`
+  align-items: center;
+  align-self: flex-start;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  margin-left: -${searchInputIconSize}px;
+  margin-top: 1rem;
+`;
+
+const TopicSearch = styled(TextInput)`
+  max-width: 350px;
+
   &:focus {
     outline: 1px solid ${palette.purple};
   }
